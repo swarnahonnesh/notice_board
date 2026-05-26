a6bf902c3789a62950d5f434f15b526550a30d85
swarnahonnesh <swarnahonnesh.birur@gmail.com>
2026-05-26 15:06:29 +0530

    Create hod_login.php

---
+++ hod_login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HoD Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="login-page-body">
+
+    <div class="login-container">
+
+        <h2>👩‍🏫 HoD Login</h2>
+
+        <form action="hod_dashboard.php" method="post">
+
+            <input type="text" name="name" placeholder="Enter Name" required><br><br>
+
+            <input type="password" name="password" placeholder="Enter Password" required><br><br>
+
+            <button type="submit">Login</button>
+
+        </form>
+
+    </div>
+
+</body>
+</html>
